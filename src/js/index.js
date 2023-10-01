622caee0af202ba9a052eaeff62545480352b5ba
Gui Barros <guisanches.exe@gmail.com>
2023-10-01 19:12:52 -0300

    feat: add character change

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll(".personagem")
 
-
-botoes.forEach(botao => {
+botoes.forEach((botao, i) => {
     botao.addEventListener("click", () =>{
 
         const botaoSelecionado = document.querySelector(".botao.selecionado")
         botaoSelecionado.classList.remove("selecionado")
-        
+
         console.log(botao.classList.add("selecionado"))
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado")
+        personagemSelecionado.classList.remove("selecionado")
+
+        personagens[i].classList.add("selecionado")
     });
 });
